d48f89a0a12bb9ece929c3e6a3100d9752ef4832
ratkoviclazar <lazarratkovic0@gmail.com>
2025-05-12 15:57:22 +0200

    changed css settings

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+// import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
-  plugins: [react(), cssInjectedByJsPlugin()],
+  plugins: [react(), /*cssInjectedByJsPlugin()*/],
   build: {
-    cssCodeSplit: false, 
     lib: {
       entry: 'src/index.js',
       name: 'MyComponent',
